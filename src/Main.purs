593064ac58a6a72c4e1b38de5c8d1bb2449cab5d
Nathaniel May <nathaniel.may@fishtownanalytics.com>
2022-09-18 19:48:48 -0400

    init halogen

--- src/Main.purs
+++ src/Main.purs
@@ -2,44 +2,41 @@ module Main where
 
 import Prelude
 
-import Control.Monad.Error.Class (liftEither, class MonadError, throwError)
-import Control.Monad.Except.Trans (runExceptT)
+import Control.Monad.Error.Class (class MonadError, liftEither)
+import Data.DateTime (diff)
+import Data.DateTime.Instant (toDateTime)
 import Data.Either (Either(..), either)
-import Data.Maybe (Maybe, maybe)
+import Data.Maybe (Maybe(..), maybe)
+import Data.Number as Number
+import Data.String.Utils (lines)
+import Data.Time.Duration (Milliseconds)
 import Data.Traversable (sequence)
 import Data.Tuple (Tuple(..))
 import Effect (Effect)
-import Effect.Class (class MonadEffect, liftEffect)
+import Effect.Aff.Class (class MonadAff)
 import Effect.Class.Console (log)
+import Effect.Now (now)
+import Halogen as H
+import Halogen.Aff as HA
+import Halogen.Component (Component)
+import Halogen.HTML as HH
+import Halogen.HTML.Core (HTML)
+import Halogen.HTML.Events as HE
+import Halogen.HTML.Properties as HP
+import Halogen.VDom.Driver (runUI)
 import MonteCarlo (confidenceInterval, sample)
-import Probability (probability)
+import Probability (p90, p95, p99, p999, probability, Probability)
+import SortedArray (SortedArray)
 import SortedArray as SortedArray
 
 
-main :: Effect Unit
-main = do 
-  result <- runExceptT app
-  either log pure result
-
-app :: ∀ m. Monad m => MonadError String m => MonadEffect m => m Unit
-app = do
-  let sampleSize = 1000000
-  log $ "Running " <> show sampleSize <> " experiments..."
-  let attendanceNumbers = [0.1, 0.9, 0.9, 0.99]  -- TODO dummy data
-  attendanceDist <- either
-    (throwError <<< probErr)
-    pure
-    (sequence $ probability <$> attendanceNumbers)
-  samples <- liftEffect $ sample attendanceDist sampleSize
-  log "sorting samples..."
-  let sortedSamples = SortedArray.fromArray samples
-  log "99.9% Confidence Interval for Attendance:"
-  p999 <- either (throwError <<< probErr) pure (probability 0.999)
-  result <- noteT "error computing confidence interval" (confidenceInterval p999 sortedSamples)
-  log $ showTuple result
+data Tuple4 a b c d = Tuple4 a b c d
 
 showTuple :: ∀ a b. Show a => Show b => Tuple a b -> String
 showTuple (Tuple a b) = "(" <> show a <> ", " <> show b <> ")"
+
+showTuple4 :: ∀ a b c d. Show a => Show b => Show c => Show d => Tuple4 a b c d -> String
+showTuple4 (Tuple4 a b c d) = "(" <> show a <> ", " <> show b <> ", " <> show c <> ", " <> show d <> ")"
 
 probErr :: Number -> String
 probErr n = "error parsing distribution probabilities: " <> show n <> " is not between 0.0 and 1.0"
@@ -52,3 +49,142 @@ note e = maybe (Left e) Right
 
 noteT :: ∀ e m a. MonadError e m => e -> Maybe a -> m a
 noteT e = liftEither <<< note e
+
+
+---- Halogen ---- 
+
+main :: Effect Unit
+main = HA.runHalogenAff do
+  body <- HA.awaitBody
+  runUI component unit body
+
+type State =
+  { dist :: Maybe (SortedArray Int)
+  , loading :: Boolean
+  , input :: String
+  , parsed :: Maybe (Array Probability)
+  , result :: Maybe ({ p90 :: Tuple Int Int 
+                     , p95 :: Tuple Int Int 
+                     , p99 :: Tuple Int Int 
+                     , p999 :: Tuple Int Int 
+                     })
+  , error :: Maybe (Error)
+  }
+
+data Error
+    = InvalidNumber String
+    | InvalidProbability Number
+    | InternalError
+
+data Action 
+    = RunExperiments
+    | Parse
+
+component :: ∀ m a b c. MonadAff m => Component a b c m
+component =
+  H.mkComponent
+    { initialState
+    , render
+    , eval: H.mkEval $ H.defaultEval { handleAction = handleAction }
+    }
+
+initialState :: ∀ i. i -> State
+initialState _ = 
+  { dist: Nothing
+  , loading: false
+  , input: ""
+  , parsed: Nothing
+  , result: Nothing
+  , error: Nothing
+  }
+
+parse :: String -> Either Error (Array Probability)
+parse s = do
+  nums <- sequence $ parseNum <$> lines s
+  sequence $ (mapLeft InvalidProbability <<< probability) <$> nums
+
+parseNum :: String -> Either Error Number
+parseNum s = maybe (Left $ InvalidNumber s) Right (Number.fromString s)
+
+render :: ∀ a. State -> HTML a Action
+render st =
+    HH.div
+        [ HP.class_ (H.ClassName "VContainer") ]
+        [ HH.div_
+            [ HH.div
+                [ HP.class_ (H.ClassName "HContainer") ]
+                [ HH.h1_ [ HH.text "Party Carlo" ] ]
+            , HH.div
+                [ HP.class_ (H.ClassName "VContainer") ] 
+                [ HH.button
+                    [ HP.disabled st.loading
+                    , HP.id "RunExperiments"
+                    , HP.type_ HP.ButtonButton
+                    , HE.onClick (\_ -> RunExperiments)
+                    ]
+                    [ HH.text if st.loading then "Experimenting..." else "Run Experiments" ]
+                ]
+            , HH.div 
+                [ HP.class_ (H.ClassName "results") ] 
+                [ HH.h2_ [ HH.text "90% - TODO" ] 
+                , HH.h2_ [ HH.text "95% - TODO" ] 
+                , HH.h2_ [ HH.text "99% - TODO" ] 
+                , HH.h2_ [ HH.text "99.9% - TODO" ] 
+                ]
+            , HH.div
+                [ HP.class_ (H.ClassName "input") ]  
+                [ HH.textarea
+                    [ HP.disabled st.loading
+                    , HP.id "input"
+                    , HP.value st.input
+                    , HE.onFocusOut (\_ -> Parse)
+                    ]
+                ]
+            , HH.div
+                [ HP.class_ (H.ClassName "VContainer") ]
+                [ HH.footer_
+                    [ HH.text "PureScript + Netlify | Source on " 
+                    , HH.a 
+                        [ HP.href "https://github.com/nathaniel-may/party-carlo" ] 
+                        [ HH.text "GitHub" ]
+                    ]
+                ]
+            ]
+        ]
+
+handleAction :: ∀ o m. MonadAff m => Action -> H.HalogenM State Action () o m Unit
+
+handleAction Parse = do
+    st <- H.get
+    case parse st.input of
+        Left e -> H.modify_ (_ { error = Just e })
+        Right parsed -> H.modify_ (_ { parsed = Just parsed })
+
+handleAction RunExperiments = do
+    st <- H.get
+    let count = 1000000
+    case st.parsed of
+        Nothing -> pure unit
+        Just dist -> do
+            log $ "running " <> show count <> " experiments ..."
+            H.modify_ (_ { loading = true })
+            start <- H.liftEffect $ map toDateTime now
+            samples <- H.liftEffect $ sample dist count
+            let sorted = SortedArray.fromArray samples
+            H.modify_ (_ { dist = Just sorted })
+            let m4 = ( 
+                Tuple4 <$> confidenceInterval p90 sorted
+                <*> confidenceInterval p95 sorted
+                <*> confidenceInterval p99 sorted
+                <*> confidenceInterval p999 sorted)
+            case m4 of
+                Nothing -> H.modify_ (_ { error = Just InternalError })
+                Just t4@(Tuple4 p90val p95val p99val p999val) -> do
+                    H.modify_ (_ { result = Just { p90: p90val
+                                                 , p95: p95val
+                                                 , p99: p99val
+                                                 , p999: p999val } })
+                    H.modify_ (_ { loading = false, error = Nothing })
+                    end <- H.liftEffect $ map toDateTime now
+                    log $ "result calculated in " <> show (diff end start :: Milliseconds) <> ":"
+                    log $ showTuple4 t4
--- src/Probability.purs
+++ src/Probability.purs
@@ -1,5 +1,9 @@
 module Probability (
     Probability
+  , p90
+  , p95
+  , p99
+  , p999
   , probability
   , toNumber
 ) where
@@ -18,6 +22,19 @@ probability p = if p <= 1.0 && p >= 0.0 then Right (Probability p) else Left p
 
 toNumber :: Probability -> Number
 toNumber (Probability x) = x
+
+-- commonly referenced probabilities in source
+p90 :: Probability
+p90 = Probability 0.9
+
+p95 :: Probability
+p95 = Probability 0.95
+
+p99 :: Probability
+p99 = Probability 0.99
+
+p999 :: Probability
+p999 = Probability 0.999
 
 instance arbProbability :: Arbitrary Probability where
   arbitrary = Probability <$> (choose 0.0 1.0)
