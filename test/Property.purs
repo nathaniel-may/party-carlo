56877343d44c63315c21713e267edfd59d2eaae1
Nathaniel May <nathaniel.may@fishtownanalytics.com>
2022-11-22 14:37:31 -0500

    add smoke test for rng

--- test/Property.purs
+++ test/Property.purs
@@ -6,13 +6,16 @@ import Prelude
 
 import Data.Foldable (sum)
 import Data.Int (round)
+import Data.Int as Int
 import Data.Maybe (Maybe(..))
+import Data.Ord (abs)
 import Data.Tuple (Tuple(..))
 import Effect (Effect)
 import PartyCarlo.Capability.Pack (class Pack)
 import PartyCarlo.Data.Probability (Probability)
 import PartyCarlo.Data.Probability as Prob
 import PartyCarlo.MonteCarlo (Dist, monteCarloConfidenceInterval)
+import PartyCarlo.Utils (count, rngs)
 import Random.PseudoRandom (Seed)
 import Test.PropTestM (runPropTestM)
 import Test.QuickCheck (Result, quickCheck, (<?>))
@@ -20,7 +23,7 @@ import Test.Utils (SeedGen(..))
 
 
 allTests :: Array (Effect Unit)
-allTests = quickCheck <$> [ test0 ]
+allTests = [ quickCheck test0, quickCheck test1 ]
     where
 
     test0 :: SeedGen -> Probability -> Dist -> Result
@@ -44,3 +47,23 @@ allTests = quickCheck <$> [ test0 ]
                         <> " p=" <> (show $ Prob.toNumber p)
                         <> " dist=" <> (show $ Prob.toNumber <$> dist)
                         <> " rngSeed=" <> (show seed))
+
+    -- | a smoke test for if a mistake is made when swapping out the rng
+    test1 :: SeedGen -> Result
+    test1 (SeedGen seed) = runPropTestM test seed 
+        where 
+            test :: ∀ m. Pack Seed m => m Result
+            test = do
+                let n = 1000
+                let threshold = 0.1
+                xs <- rngs n
+                let lower = count (\x -> Prob.toNumber x < 0.5) xs
+                let upper = n - lower
+                let maxDiff = Int.toNumber n * threshold
+                pure $ (Int.toNumber $ abs (lower - upper)) < maxDiff
+                    <?> ("random number generator is generating lop-sided sequences. Out of "
+                    <> show n <> " generated probabilities " 
+                    <> show lower <> " were < 0.5 and "
+                    <> show upper <> " were >= 0.5 which is above the threshold of a difference of "
+                    <> show maxDiff)
+
